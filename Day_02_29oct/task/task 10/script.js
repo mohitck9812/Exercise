fcc66694d3d909157a0d52ac87bd8c1963dda88c
mohitCK <mohit@cloudkeeper.com>
2025-11-02 10:56:40 +0530

    Made changes in folder structure

--- Day_02_29oct/task/task 10/script.js
+++ Day_02_29oct/task/task 10/script.js
@@ -1,5 +1,37 @@
 const addStudent = document.getElementById('addStudent');
 const table = document.getElementById('tableBody');
+const localData = JSON.parse(localStorage.getItem("studentData") )|| [];
+
+
+// to eedit this 
+
+function studentEntry(studentData){
+    const studentName = studentData.name;
+    const [hindiMarks, englishMarks, mathMarks, scienceMarks, ssMarks, totalMarks, average] = studentData.value;
+
+    const newRow = document.createElement("tr");
+
+    newRow.innerHTML += `
+        <td>${studentName}</td>
+        <td>${hindiMarks}</td>
+        <td>${englishMarks}</td>
+        <td>${mathMarks}</td>
+        <td>${scienceMarks}</td>
+        <td>${ssMarks}</td>
+        <td>${totalMarks}</td>
+        <td>${average}</td>
+    `;
+
+    localData.push({studentName : marks});
+
+    localStorage.setItem("studentData",JSON.stringify(localData));
+
+    table.appendChild(newRow);
+}
+
+// localData.array.forEach(element => {
+//     studentEntry()
+// });
 
 addStudent.addEventListener('click', () => {
     console.log("add button clicked");
@@ -28,19 +60,10 @@ addStudent.addEventListener('click', () => {
     const totalMarks = hindiMarks + englishMarks + mathMarks + scienceMarks + ssMarks;
     const average = (totalMarks/5).toFixed(2);
     
-    const newRow = document.createElement("tr");
+    const studentData = {marks : [hindiMarks, englishMarks, mathMarks, scienceMarks,ssMarks, totalMarks, average],
+        name: studentName
+    }
 
-    newRow.innerHTML += `
-        <td>${studentName}</td>
-        <td>${hindiMarks}</td>
-        <td>${englishMarks}</td>
-        <td>${mathMarks}</td>
-        <td>${scienceMarks}</td>
-        <td>${ssMarks}</td>
-        <td>${totalMarks}</td>
-        <td>${average}</td>
-    `;
-
-    table.appendChild(newRow);
+    studentEntry(studentData);
 
 })
